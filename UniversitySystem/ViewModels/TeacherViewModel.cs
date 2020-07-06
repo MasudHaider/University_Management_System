4cc7732b2a37db5ee2a3b80ecff1b3ed7c943598
MasudHaider <masud6563@gmail.com>
2020-07-06 22:04:33 +0600

    teacher viewmodel to generate view with required fields

---
+++ UniversitySystem/ViewModels/TeacherViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversitySystem.ViewModels
+{
+    public class TeacherViewModel
+    {
+        public string TeacherName { get; set; }
+
+        public string TeacherAddress { get; set; }
+
+        public string TeacherEmail { get; set; }
+
+        public string TeacherContactNumber { get; set; }
+
+        public int SelectedDesignationId { get; set; }
+        public IEnumerable<SelectListItem> Designations { get; set; }
+
+        public int SelectedDepartmentId { get; set; }
+        public IEnumerable<SelectListItem> Departments { get; set; }
+
+        public float TeacherCredits { get; set; }
+    }
+}
