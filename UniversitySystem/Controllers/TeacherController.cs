34a6232cabc5f18d9af6247f62539e2f244d30b0
MasudHaider <masud6563@gmail.com>
2020-07-06 22:07:13 +0600

    save teacher view generated by viewmodel passed from controller

---
+++ UniversitySystem/Controllers/TeacherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversitySystem.Models;
+using UniversitySystem.ViewModels;
+
+namespace UniversitySystem.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeacherController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool dispose)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult SaveTeacher()
+        {
+            var department = new Department();
+
+            var teacherViewModel = new TeacherViewModel
+            {
+                Departments = department.GetDepartments()
+            };
+
+            return View(teacherViewModel);
+        }
+    }
+}
